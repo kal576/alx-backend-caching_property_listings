a11aa9af315504e0a000563abc33cbaf7444e31a
kal576 <cmuuo5864@gmail.com>
2025-07-10 18:55:28 +0300

    Add caching

--- properties/views.py
+++ properties/views.py
@@ -2,7 +2,7 @@
 
 from django.shortcuts import render
 from .models import Property
-from rest_framework.decorators import cache_page
+from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
 
 @cache_page(60 * 15)
