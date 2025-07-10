26446f2aa904c5d02fbaf942fc2c5e44a0fb8739
kal576 <cmuuo5864@gmail.com>
2025-07-10 20:04:35 +0300

    Add signals.py

--- properties/utils.py
+++ properties/utils.py
@@ -1,7 +1,36 @@
 from django.core.cache import cache
 from .models import Property
+import logging
+from django_redis import get_redis_connetion
 
 def get_all_properties():
+    try:
+        conn = get_redis_connection("default")
+        info = conn.info()
+
+        hits = info.get("keyspace_hits", 0)
+        misses = info.get("keyspace_misses", 0)
+        total = hits + misses
+
+        hit_ratio = (hit / total) if total > 0 else 0.0
+
+        metrics = {
+                "hits": hits,
+                "misses": misses,
+                "hit_ratio": round(hit_ratio, 4),
+                }
+
+        logger.info(f"Redis Cache Metrics: {metrics}")
+        return metrics
+
+    exception Exception as e:
+        logger.error(f"Failed to get Redis Cache metrics: {e}")
+        return {
+                "hits": 0,
+                "misses": 0,
+                "hit_ratio": 0,
+                "error": str{e}}
+
     properties = cache.get('all_properties')
 
     if properties is None:
