384f16bd6d5bb171031eb0ac171a1f396193fbd0
kal576 <cmuuo5864@gmail.com>
2025-07-10 18:53:55 +0300

    Add caching

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+#property view that caches the response in Redis for 15 minutes
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Property
+from rest_framework.decorators import cache_page
+from django.http import JsonResponse
+
+@cache_page(60 * 15)
+def property_list(request):
+    properties = Property.objects.all().values
+    return JsonResponse(list(properties))
+
+
